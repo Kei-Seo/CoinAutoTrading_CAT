1565b9aa40d0c808268883b9efb50f38a92ef2d5
Kei-Seo <ghdtjr3824@naver.com>
2022-01-09 19:00:39 +0900

    add 5:

--- upAlarm.py
+++ upAlarm.py
@@ -3,9 +3,9 @@ import time
 import pybithumb
 
 tickers = pybithumb.get_tickers()
+btc = pybithumb.get_ohlcv("BTC")
+close = btc['close']
 
-print(tickers)
-while True:
-    price = pybithumb.get_current_price("BTC")
-    print(price)
-    time.sleep(1)
+print((close[0] + close[1] + close[2] + close[3] + close[4]) / 5)
+print((close[1] + close[2] + close[3] + close[4] + close[5]) / 5)
+print((close[2] + close[3] + close[4] + close[5] + close[6]) / 5)
